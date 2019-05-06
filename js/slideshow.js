7839ca3e7be74cc2da7a1e28cf33724ff72dc074
Karelle Hofler <karellehofler@gmail.com>
2019-05-06 11:43:05 -0400

    Updated css, js, html and img files

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,38 +1,26 @@
-function createSlideShow(imgSrc, img1){
-    //Section Div
-    var sectionDiv = document.getElementById("tour");
-    // The .slideshow class
-    var slideShowClass = document.createAttribute("class");
-    //the .slideshow div
-    var slideShowDiv = document.createElement("div");
-    //Adds "slideshow" value to class
-    slideShowClass.value = "slideshow"
-    //Assigns the class to the div
-    slideShowDiv.setAttributeNode(slideShowClass);
-    //Adds the slideshow div to the section div
-    sectionDiv.appendChild(slideShowDiv);
+var slideIndex = 1;
 
-    //Slideshow Image Div
-    var imgDiv = document.createElement("img");
-    var imgClass = document.createAttribute("class", "src");
-    imgClass.value = "gallery-img";
-
-    imgDiv.setAttributeNode(imgClass);
-    //Insert Image Div into slideshow     
-    slideShowDiv.appendChild(imgDiv);
-
-    imgDiv.src = "img/libary-with-fireplace.jpg";
-
-    //SLideshow button
-    var btnDiv = document.createElement("button");
-    var btnClass = document.createAttribute("class");
-    var btns = document.createElement("div");
-
-    btnClass.value("img-btn");
-    btns.insertAdjacentHTML(imgDiv);
-
-    
+function plusSlides(n) {
+    showSlideShow(slideIndex += n);
 }
 
-createSlideShow();
+function currentSlide(n) {
+    showSlideShow(slideIndex = n);
+}
+
+function showSlideShow(n){
+
+    var slides = document.getElementsByClassName("slides");
+    if(n < 1){
+        slideIndex = 1
+    }
+    if(n < 1){
+        slideIndex = slides.length
+    }
+    slides[slideIndex-1].style.display = "block";
+    
+
+}
+
+showSlideShow();
 
